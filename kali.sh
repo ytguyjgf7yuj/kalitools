07d73225d8bcc45f15688a81fc922728c119e64f
ytguyjgf7yuj <es095567@gmail.com>
2017-10-08 23:43:10 -0300

    Add files via upload

---
+++ kali.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt-get install figlet
+figlet KaliTootns
+
+apt-get install proxychains
+apt-get install openvpn
+apt-get install nmap
+apt-get install sqlmap
+apt-get install t50
+apt-get install aircrack-ng
+apt-get install fierce
+apt-get install host
+apt-get install whois
+apt-get install wifite
+apt-get install wpscan
+apt-get install python
+apt-get install python2
+apt-get install unzip
+apt-get install perl
+apt-get install dirb
+apt-get install uniscan
+apt-get install bluetooth
+apt-get install net-tools
+apt-get install dnsenum
+apt-get install ftp
+apt-get install ssh
+apt-get install apache2
+apt-get install sslstrip
+apt-get install thehavester
+apt-get install iptables
+apt-get install ettercap-text-only
+apt-get install crunch
+apt-get install john
+apt-get install netcat
+apt-get install sudo
+apt-get install ruby
